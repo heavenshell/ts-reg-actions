9fd9a5bfa609c9955cde097061d5da066145edcd
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2023-11-26 22:52:37 +0900

    First import

---
+++ README.md
@@ -0,0 +1,1 @@
+# ts-reg-actions
---
+++ stories/Button.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+    screenshot: {
+      waitFor: 'myWait',
+      captureBeyondViewport: false,
+      viewport: {
+        width: 400,
+        height: 200,
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof Button>;
+
+type Story = StoryObj<typeof meta>;
+
+export const Regression: Story = {
+  args: {
+    primary: true,
+    label: 'Regression',
+    size: 'large',
+  },
+};
+
+export default meta;
